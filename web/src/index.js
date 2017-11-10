4d8ab22fb7f6e34679c78e8ff10ae1b636605bf0
Tian Zhang <975571814@qq.com>
2017-11-10 17:03:42 +0800

    add redux to project

--- web/src/index.js
+++ web/src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducer from './reducers/index.js';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+const store = createStoreWithMiddleware(reducer);
+
+ReactDOM.render(<Provider store={store}>
+    <Router>
+        <div>
+            <Route path="/" component={App}/>
+        </div>
+    </Router>
+</Provider>, document.getElementById('root'));
 registerServiceWorker();
