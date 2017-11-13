89e34671fee046d52278616fbeb284b934702b62
Tian Zhang <975571814@qq.com>
2017-11-13 20:12:32 +0800

    write get diaries and delete api

--- backend/src/main/java/com/tw/controllers/DiaryController.java
+++ backend/src/main/java/com/tw/controllers/DiaryController.java
@@ -6,10 +6,9 @@ import com.tw.entities.Diary;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class DiaryController {
@@ -20,15 +19,32 @@ public class DiaryController {
     @RequestMapping(value = "/api/diary", method = RequestMethod.POST)
     public ResponseEntity addDiary(@RequestBody Diary diary) throws Exception {
 
-        System.out.print("=============");
         Diary back = diaryRepository.save(diary);
 
         if (!diaryRepository.exists(back.getId())) {
             throw new AppearException("保存失败！");
         }
-        ResponseEntity a = new ResponseEntity(HttpStatus.CREATED);
+        ResponseEntity a = new ResponseEntity<>(HttpStatus.CREATED);
         System.out.print(a);
 
         return a;
+    }
+
+    @RequestMapping(value = "/api/diary/{id}", method = RequestMethod.GET)
+    public ResponseEntity getDiary(@PathVariable("id") Long id) {
+        List<Diary> diaries = diaryRepository.findByUserId(id);
+
+        return new ResponseEntity<>(diaries, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/api/diary/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity deleteDiary(@PathVariable("id") Long id) throws Exception {
+        diaryRepository.delete(id);
+
+        if (diaryRepository.exists(id)) {
+            throw new AppearException("删除失败！");
+        }
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- backend/src/main/java/com/tw/repositories/DiaryRepository.java
+++ backend/src/main/java/com/tw/repositories/DiaryRepository.java
@@ -3,6 +3,9 @@ package com.tw.repositories;
 import com.tw.entities.Diary;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DiaryRepository extends JpaRepository<Diary,Long>{
 
+    List<Diary> findByUserId(Long id);
 }
